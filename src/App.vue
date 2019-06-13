c846c50cc73adb6c67b414f92f1abd83b762f3b3
hwadii <wadii.hajji@etu.univ-amu.fr>
2019-06-14 00:31:29 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app id="inspire">
+      <Chat />
+    </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Chat from "./components/Chat";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Chat
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Open Sans", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Chat.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-container fluid pa-0 grid-list-xl overflow-x-hidden>
+    <!-- <v-toolbar>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Chat</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar> -->
+    <v-container>
+      <v-layout pa-3 align-center justify-start fill-height column>
+        <v-flex xs12>
+          <v-card dark color="primary">
+            <v-card-text>Hello</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs2>
+          <v-card dark color="secondary">
+            <v-card-text>Hi</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs2>
+          <v-card dark color="primary">
+            <v-card-text>Hello</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container fluid class="send">
+      <v-layout align-end justify-center row fill-height>
+        <v-textarea
+          @click:append="sayHi()"
+          rows="1"
+          hide-details
+          solo
+          append-icon="send"
+          auto-grow
+          placeholder="Aa"
+        ></v-textarea>
+      </v-layout>
+    </v-container>
+  </v-container>
+</template>
+
+<style>
+.send {
+  position: fixed;
+  bottom: 0;
+}
+</style>
+
+
+<script>
+export default {
+  name: "Chat",
+  data: function() {
+    return {
+      m: 2
+    };
+  },
+  methods: {
+    sayHi: () => {
+      return "hi";
+    }
+  }
+};
+</script>
+
