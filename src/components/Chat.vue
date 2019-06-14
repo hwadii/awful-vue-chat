65979a466719ae2840fb7a6c3352f0b0ef4b74aa
Your Name <you@example.com>
2019-06-14 16:56:31 +0200

    chat update

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,37 +1,24 @@
 <template>
-  <v-container fluid pa-0 grid-list-xl overflow-x-hidden>
-    <!-- <v-toolbar>
-      <v-spacer></v-spacer>
-      <v-toolbar-title>Chat</v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar> -->
-    <v-container>
-      <v-layout pa-3 align-center justify-start fill-height column>
-        <v-flex xs12>
-          <v-card dark color="primary">
-            <v-card-text>Hello</v-card-text>
-          </v-card>
-        </v-flex>
-        <v-flex xs2>
-          <v-card dark color="secondary">
-            <v-card-text>Hi</v-card-text>
-          </v-card>
-        </v-flex>
-        <v-flex xs2>
-          <v-card dark color="primary">
-            <v-card-text>Hello</v-card-text>
-          </v-card>
+  <v-container fluid pa-0 grid-list-md>
+    <v-container class="feed">
+      <v-layout pa-2 mb-2 column wrap>
+        <v-flex v-for="(msg, i) in feed" :key="i">
+          <div v-if="msg.id == 0" class="speech-bubble-right">{{ msg.content }}</div>
+          <div v-else class="speech-bubble-left">{{ msg.content }}</div>
         </v-flex>
       </v-layout>
     </v-container>
-    <v-container fluid class="send">
+    <v-container fluid pa-1 class="send" overflow-hidden>
       <v-layout align-end justify-center row fill-height>
         <v-textarea
-          @click:append="sayHi()"
+          v-model="content"
+          @click:append="sendMessage({id: 0, name: 'Me', content: content})"
+          @click:prepend-inner="sendMessage(mock)"
           rows="1"
           hide-details
           solo
           append-icon="send"
+          prepend-inner-icon="expand_more"
           auto-grow
           placeholder="Aa"
         ></v-textarea>
@@ -45,6 +32,54 @@
   position: fixed;
   bottom: 0;
 }
+.speech-bubble-right {
+  max-width: 80%;
+  float: right;
+  padding: 2px 10%;
+  margin-bottom: 1em;
+  position: relative;
+  background: white;
+  border-radius: 0.4em;
+}
+
+.speech-bubble-right:after {
+  content: "";
+  position: absolute;
+  right: 0;
+  top: 60%;
+  width: 0;
+  height: 0;
+  border: 0.969em solid transparent;
+  border-left-color: white;
+  border-right: 0;
+  border-bottom: 0;
+  margin-top: -0.484em;
+  margin-right: -0.969em;
+}
+
+.speech-bubble-left {
+  max-width: 80%;
+  float: left;
+  padding: 2px 10%;
+  position: relative;
+  background: lime;
+  border-radius: 0.4em;
+}
+
+.speech-bubble-left:after {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 60%;
+  width: 0;
+  height: 0;
+  border: 0.969em solid transparent;
+  border-right-color: lime;
+  border-left: 0;
+  border-bottom: 0;
+  margin-top: -0.484em;
+  margin-left: -0.969em;
+}
 </style>
 
 
@@ -53,12 +88,39 @@ export default {
   name: "Chat",
   data: function() {
     return {
-      m: 2
+      content: "",
+      mock: {
+        id: 1,
+        name: "Person",
+        content: "Fake message"
+      },
+      feed: [
+        {
+          id: 0,
+          name: "Me",
+          content:
+            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        },
+        {
+          id: 1,
+          name: "Person",
+          content: "Hi"
+        },
+        {
+          id: 0,
+          name: "Me",
+          content: "Hi"
+        }
+      ]
     };
   },
   methods: {
-    sayHi: () => {
-      return "hi";
+    sendMessage: function(msg) {
+      if (/^\s*$/.exec(msg.content) == null) {
+        this.feed.push({ id: msg.id, name: msg.name, content: msg.content });
+        this.content = "";
+        this.$vuetify.goTo(9999, "easeOutQuad");
+      }
     }
   }
 };
